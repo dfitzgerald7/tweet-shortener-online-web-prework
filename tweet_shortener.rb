97b96845675fe93a3bc836387c4406a61b7bccaa
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-17 21:48:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,7 +26,11 @@ def bulk_tweet_shortener(array)
   end 
 end 
 
-def selective_tweet_sortener
-  
+def selective_tweet_sortener(tweet)
+  if tweet.size > 140 
+    puts word_substituter(tweet)
+  else
+    puts tweet
+  end 
 end 
 

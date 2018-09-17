33ab8328889de9a2dd142e571273639959c5f874
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-17 21:41:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
+require "pry"
 # Write your code here.
+def dictionary 
+  my_hash = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for"=> "4", "four"=> "4", "be" => "b","you" => "u", "at" => "@", "and" => "&"} 
+end 
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  dictionary_array = dictionary 
+  long_words = dictionary_array.keys
+  tweet_array.collect! do |word|
+    if long_words.include?(word)
+      word = dictionary_array[word]      
+    else 
+      word = word 
+    end 
+  
+  end 
+ 
+  tweet_array.join(" ")
+end 
+
